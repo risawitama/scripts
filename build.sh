dd46972bcc54b205b0b79cfde90b6ae36e9b86f4
koko <ko282ko82@gmail.com>
2024-10-05 14:14:02 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ export ARCH=arm64
 export KBUILD_BUILD_HOST=linux-build
 export KBUILD_BUILD_USER="stim"
 clangbin=clang/bin/clang
-if ! [ -a $clangbin ]; then git clone --depth=1 https://gitlab.com/kei-space/clang/r522817.git clang
+if ! [ -a $clangbin ]; then git clone --depth=1 https://gitlab.com/inferno0230/clang-r487747c.git clang
 fi
 rm -rf AnyKernel
 make O=out ARCH=arm64 vendor/lineage-a52q_defconfig
