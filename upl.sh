3d2c6958d4ab15174018c3c4f9dbbc76cc22ee39
koko <ko282ko82@gmail.com>
2024-10-07 23:19:56 +0530

    Update upl.sh

--- upl.sh
+++ upl.sh
@@ -4,20 +4,17 @@ DIRROM="Pixel14/"
 # Check if the directory exists
 if [ -d "$DIRROM" ]; then
     echo "Directory $DIRROM exists."
+    cd "$DIRROM"
+    rm .repo/manifests/crave.yaml* || true; # Removes existing crave.yamls
+    curl -o .repo/manifests/crave.yaml https://raw.githubusercontent.com/sounddrill31/crave_aosp_builder/main/configs/crave/crave.yaml.aosp # Downloads crave.yaml
+    echo "--------------cloned crave yaml---------------"
+    crave pull out/target/product/a52q/*.zip
 else
     echo "Directory $DIRROM does not exist.creating."
     crave clone create --projectID 82 /crave-devspaces/Pixel14
+    cd "$DIRROM"
+    rm .repo/manifests/crave.yaml* || true; # Removes existing crave.yamls
+    curl -o .repo/manifests/crave.yaml https://raw.githubusercontent.com/sounddrill31/crave_aosp_builder/main/configs/crave/crave.yaml.aosp # Downloads crave.yaml
+    echo "--------------cloned crave yaml---------------"
+    crave pull out/target/product/a52q/*.zip
 fi
-
-
-#crave yaml
-rm pos/.repo/manifests/crave.yaml* || true; # Removes existing crave.yamls
-
-curl -o Pixel14/.repo/manifests/crave.yaml https://raw.githubusercontent.com/sounddrill31/crave_aosp_builder/main/configs/crave/crave.yaml.aosp # Downloads crave.yaml
-
-echo "--------------cloned crave yaml---------------"
-
-
-crave pull Pixel14/out/target/product/a52q/*.zip
-
-echo "---------------pulled rom zip----------------"
