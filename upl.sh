6ecb2a278a60a1901b0466cbb4dc7aa74130e90a
koko <ko282ko82@gmail.com>
2024-10-07 23:16:16 +0530

    Update upl.sh

--- upl.sh
+++ upl.sh
@@ -3,33 +3,21 @@
 DIRROM="Pixel14/"
 # Check if the directory exists
 if [ -d "$DIRROM" ]; then
-    echo "Directory $DIRROM exists. Deleting it..."
-    crave clone destroy "$DIRROM" -y
-    echo "Directory deleted."
+    echo "Directory $DIRROM exists."
 else
-    echo "Directory $DIRROM does not exist. No need to delete."
+    echo "Directory $DIRROM does not exist.creating."
+    crave clone create --projectID 82 /crave-devspaces/Pixel14
 fi
-
-#Create Project Dir
-crave clone create --projectID 82 /crave-devspaces/Pixel14
-
-echo "---------Project dir Created------------"
-
-
-#Go into the dir
-cd Pixel14
-
-echo "-----------Inside the Project dir-------------"
 
 
 #crave yaml
-rm .repo/manifests/crave.yaml* || true; # Removes existing crave.yamls
+rm pos/.repo/manifests/crave.yaml* || true; # Removes existing crave.yamls
 
-curl -o .repo/manifests/crave.yaml https://raw.githubusercontent.com/sounddrill31/crave_aosp_builder/main/configs/crave/crave.yaml.aosp # Downloads crave.yaml
+curl -o Pixel14/.repo/manifests/crave.yaml https://raw.githubusercontent.com/sounddrill31/crave_aosp_builder/main/configs/crave/crave.yaml.aosp # Downloads crave.yaml
 
 echo "--------------cloned crave yaml---------------"
 
 
-crave pull out/target/product/a52q/*.zip
+crave pull Pixel14/out/target/product/a52q/*.zip
 
 echo "---------------pulled rom zip----------------"
