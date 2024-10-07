1125aa8b48bfff83d155266dab99602a27038341
koko <ko282ko82@gmail.com>
2024-10-07 23:35:35 +0530

    Update upl.sh

--- upl.sh
+++ upl.sh
@@ -9,7 +9,7 @@ if [ -d "$DIRROM" ]; then
     echo "--------------cloned crave yaml---------------"
     crave pull "$DIRROM"/out/target/product/a52q/*.zip
 else
-    echo "Directory $DIRROM does not exist.creating."
+    echo "Directory $DIRROM does not exist. now creating."
     crave clone create --projectID 82 /crave-devspaces/Pixel14
     rm "$DIRROM"/.repo/manifests/crave.yaml* || true; # Removes existing crave.yamls
     curl -o "$DIRROM".repo/manifests/crave.yaml https://raw.githubusercontent.com/sounddrill31/crave_aosp_builder/main/configs/crave/crave.yaml.aosp # Downloads crave.yaml
