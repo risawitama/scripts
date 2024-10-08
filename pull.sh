cd0280367ef22b5ecd0c4a7678526e9e9ddbc09d
koko <ko282ko82@gmail.com>
2024-10-08 11:32:38 +0530

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -9,6 +9,8 @@ if [ -d "$DIRROM" ]; then
     curl -o .repo/manifests/crave.yaml https://raw.githubusercontent.com/sounddrill31/crave_aosp_builder/main/configs/crave/crave.yaml.aosp # Downloads crave.yaml
     echo "--------------cloned crave yaml---------------"
     crave pull out/target/product/a52q/*.zip
+    crave pull out/build_date.txt
+    echo "-----done------"
 else
     echo "Directory $DIRROM does not exist. now creating."
     crave clone create --projectID 82 /crave-devspaces/Pixel14
@@ -17,4 +19,6 @@ else
     curl -o .repo/manifests/crave.yaml https://raw.githubusercontent.com/sounddrill31/crave_aosp_builder/main/configs/crave/crave.yaml.aosp # Downloads crave.yaml
     echo "--------------cloned crave yaml---------------"
     crave pull out/target/product/a52q/*.zip
+    crave pull out/build_date.txt
+    echo "-----done------"
 fi
