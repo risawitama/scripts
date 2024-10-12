8cf45fffa32bc5943493fe115d1536b72ac14e5b
koko <ko282ko82@gmail.com>
2024-10-12 16:17:07 +0530

    Update kernel_build.sh

--- kernel_build.sh
+++ kernel_build.sh
@@ -7,9 +7,9 @@ ccache -M 25G
 TANGGAL=$(date +"%Y%m%d-%H")
 export ARCH=arm64
 export KBUILD_BUILD_HOST=linux-build
-export KBUILD_BUILD_USER="stim"
+export KBUILD_BUILD_USER="koko"
 clangbin=clang/bin/clang
-if ! [ -a $clangbin ]; then git clone --depth=1 https://gitlab.com/kei-space/clang/r522817.git clang
+if ! [ -a $clangbin ]; then git clone --depth=1 https://gitlab.com/inferno0230/clang-r487747c.git clang
 fi
 rm -rf AnyKernel
 make O=out ARCH=arm64 vendor/lineage-a52q_defconfig
@@ -38,7 +38,7 @@ echo -e " Kernel Compile Successful"
 git clone --depth=1 https://github.com/StimLuks87/AnyKernel3.git AnyKernel
 cp out/arch/arm64/boot/Image.gz AnyKernel
 cd AnyKernel
-zip -r9 Lineage-RM6785-${TANGGAL}.zip *
+zip -r9 Spark-1.1-A14-${TANGGAL}.zip *
 cd ../
 fi
 }
