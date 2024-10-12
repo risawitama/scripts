f78eb2fd65cefeab9d1770cc43e99ec06b4bba65
koko <ko282ko82@gmail.com>
2024-10-12 15:53:10 +0530

    Update kernel-build.sh

--- kernel-build.sh
+++ kernel-build.sh
@@ -8,10 +8,11 @@ TANGGAL=$(date +"%Y%m%d-%H")
 export ARCH=arm64
 export KBUILD_BUILD_HOST=linux-build
 export KBUILD_BUILD_USER="koko"
-clangbin=clang/bin/clang
-if ! [ -a $clangbin ]; then git clone --depth=1 https://gitlab.com/inferno0230/clang-r487747c.git clang
+clangbin=/home/koko/pos/kernel/clang/bin/clang
+if ! [ -a $clangbin ]; then git clone --depth=1 https://gitlab.com/inferno0230/clang-r487747c.git /home/koko/pos/kernel/clang
 fi
-rm -rf AnyKernel
+rm -rf /home/koko/pos/kernel/AnyKernel
+ 
 make O=out ARCH=arm64 vendor/lineage-a52q_defconfig
 PATH="${PWD}/clang/bin:${PATH}" \
 make -j$(nproc --all) O=out \
@@ -26,7 +27,7 @@ make -j$(nproc --all) O=out \
                       CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
                       LD=ld.lld \
                       CONFIG_NO_ERROR_ON_MISMATCH=y
-}
+                      
 function zupload()
 {
 zimage=out/arch/arm64/boot/Image.gz
@@ -35,10 +36,11 @@ then
 echo  " Failed To Compile Kernel"
 else
 echo -e " Kernel Compile Successful"
-git clone --depth=1 https://github.com/StimLuks87/AnyKernel3.git AnyKernel
-cp out/arch/arm64/boot/Image.gz AnyKernel
-cd AnyKernel
-zip -r9 Spark-1.0-a52q-A15-${TANGGAL}.zip *
+git clone --depth=1 https://github.com/StimLuks87/AnyKernel3.git /home/koko/pos/kernel/AnyKernel
+cp out/arch/arm64/boot/Image.gz /home/koko/pos/kernel/AnyKernel
+cd /home/koko/pos/kernel/AnyKernel
+
+zip -r9 Spark-1.0-a52q-A14-${TANGGAL}.zip *
 cd ../
 fi
 }
