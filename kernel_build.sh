e758bf52fc604a8658298a94158a7bf9d460cf08
koko <ko282ko82@gmail.com>
2024-10-12 16:13:48 +0530

    Add files via upload

---
+++ kernel_build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+function compile()
+{
+source ~/.bashrc && source ~/.profile
+export LC_ALL=C && export USE_CCACHE=1
+ccache -M 25G
+TANGGAL=$(date +"%Y%m%d-%H")
+export ARCH=arm64
+export KBUILD_BUILD_HOST=linux-build
+export KBUILD_BUILD_USER="stim"
+clangbin=clang/bin/clang
+if ! [ -a $clangbin ]; then git clone --depth=1 https://gitlab.com/kei-space/clang/r522817.git clang
+fi
+rm -rf AnyKernel
+make O=out ARCH=arm64 vendor/lineage-a52q_defconfig
+PATH="${PWD}/clang/bin:${PATH}" \
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC="clang" \
+                      AR=llvm-ar \
+                      NM=llvm-nm \
+                      STRIP=llvm-strip \
+                      OBJCOPY=llvm-objcopy \
+                      OBJDUMP=llvm-objdump\
+                      CROSS_COMPILE=aarch64-linux-gnu-\
+                      CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                      LD=ld.lld \
+                      CONFIG_NO_ERROR_ON_MISMATCH=y
+}
+function zupload()
+{
+zimage=out/arch/arm64/boot/Image.gz
+if ! [ -a $zimage ];
+then
+echo  " Failed To Compile Kernel"
+else
+echo -e " Kernel Compile Successful"
+git clone --depth=1 https://github.com/StimLuks87/AnyKernel3.git AnyKernel
+cp out/arch/arm64/boot/Image.gz AnyKernel
+cd AnyKernel
+zip -r9 Lineage-RM6785-${TANGGAL}.zip *
+cd ../
+fi
+}
+compile
+zupload
