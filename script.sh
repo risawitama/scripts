771f57b47cc1af1dc5da8665e8c3940922e50520
Risa Witama <60031111+risawitama@users.noreply.github.com>
2024-12-13 15:25:07 +0700

    crDroid-ify!

--- script.sh
+++ script.sh
@@ -4,10 +4,10 @@
 rm -rf .repo/local_manifests
 
 # Init
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs --depth=1
+repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs --depth=1
 
 # Local Manifests
-git clone https://github.com/risawitama/local_manifests --depth=1 -b rising .repo/local_manifests
+git clone https://github.com/risawitama/local_manifests --depth=1 -b crdroid .repo/local_manifests
 
 # Sync
 /opt/crave/resync.sh
@@ -16,4 +16,4 @@ git clone https://github.com/risawitama/local_manifests --depth=1 -b rising .rep
 export BUILD_USERNAME=risawitama
 export BUILD_HOSTNAME=crave
 source build/envsetup.sh
-breakfast onclite eng && make installclean && riseup onclite eng && rise b
+breakfast onclite userdebug && brunch onclite userdebug
